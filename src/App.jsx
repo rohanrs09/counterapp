fdd1d93fab8f5b620badcf1aa50e1e403b12fd78
rohanshelke <rohan_shelke_it@moderncoe.edu.in>
2024-05-08 18:16:20 +0530

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,19 @@ function App() {
   let [counter, setCount] = useState(15);
 
   const addValue = () => {
-    if(counter < 20) {
+    if (counter < 20) {
       setCount(counter + 1);
     }
   };
 
   const removeValue = () => {
-    if(counter > 0) {
+    if (counter > 0) {
       setCount(counter - 1);
     }
+  };
+
+  const goToGitHub = () => {
+    window.location.href = "https://github.com/rohanrs09/dsbda.git";
   };
 
   return (
@@ -21,7 +25,8 @@ function App() {
       <h1>HI Everyone</h1>
       <h2>counter app : {counter}</h2>
       <button onClick={addValue}>Add Value</button>
-      <button onClick={removeValue} >Remove Value</button>
+      <button onClick={removeValue}>Remove Value</button>
+      <button onClick={goToGitHub}>GitHub Repository</button>
     </>
   );
 }
